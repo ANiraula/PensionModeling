c9dce2286b77b45e6ac62895580903760b3e7a27
Anil Niraula <niraulaanil@gmail.com>
2020-06-12 12:01:07 -0400

    Add files via upload

---
+++ Database/Filter_Data.R
@@ -0,0 +1,75 @@
+##Working With Reason Database
+#by Anil Niraula
+
+###How to Filter data###
+
+##Way [1]
+###Function "filteredData()"
+###for filtering out sample of data variable we commonly use in pension analysis
+
+#>>data is the database you are using (e.g. "reason.data")
+#>>x is "state"
+#>>y is "plan_name"
+#>>fy is starting "year"
+
+filteredData <- function(data, y, fy){
+  Plan <- data %>%
+    filter(display_name == y)
+  Plan <- Plan %>%
+    filter(year > fy-1)
+  Plan <- Plan %>%
+    select(
+      year,
+      plan_name = display_name,
+      state,
+      return_1yr = x1_year_investment_return_percentage,
+      actuarial_cost_method_in_gasb_reporting,
+      funded_ratio = actuarial_funded_ratio_percentage,
+      actuarial_valuation_date_for_gasb_schedules,
+      actuarial_valuation_report_date,
+      ava = actuarial_value_of_assets_gasb_dollar,
+      mva = market_value_of_assets_dollar,
+      mva_smooth = market_assets_reported_for_asset_smoothing,
+      aal = actuarially_accrued_liabilities_dollar,
+      tpl = total_pension_liability_dollar,
+      adec = actuarially_required_contribution_dollar,
+      adec_paid_pct = actuarially_required_contribution_paid_percentage,
+      amortizaton_method,
+      asset_valuation_method_for_gasb_reporting,
+      total_benefit_payments = total_benefits_paid_dollar,
+      benefit_payments = benefit_payments_dollar,
+      refunds = refunds_dollar,
+      admin_exp = administrative_expense_dollar,
+      cost_structure,
+      payroll = covered_payroll_dollar,
+      ee_contribution = employee_contribution_dollar,
+      ee_nc_pct = employee_normal_cost_percentage,
+      er_contribution = employer_contribution_regular_dollar,
+      er_nc_pct = employer_normal_cost_percentage,
+      er_state_contribution = employer_state_contribution_dollar,
+      er_proj_adec_pct = employers_projected_actuarial_required_contribution_percentage_of_payroll,
+      other_contribution = other_contribution_dollar,
+      other_additions = other_additions_dollar,
+      fy = fiscal_year,
+      fy_contribution = fiscal_year_of_contribution,
+      inflation_assum = inflation_rate_assumption_for_gasb_reporting,
+      arr = investment_return_assumption_for_gasb_reporting,
+      number_of_years_remaining_on_amortization_schedule,
+      payroll_growth_assumption,
+      total_amortization_payment_pct = total_amortization_payment_percentage,
+      total_contribution = total_contribution_dollar,
+      total_nc_pct = total_normal_cost_percentage,
+      total_number_of_members,
+      total_proj_adec_pct = total_projected_actuarial_required_contribution_percentage_of_payroll,
+      type_of_employees_covered,
+      unfunded_actuarially_accrued_liabilities_dollar,
+      wage_inflation)
+}
+
+###Example
+library(pensionviewr)#more info: https://github.com/ReasonFoundation/pensionviewr
+library(dplyr)
+
+urlfile="https://raw.githubusercontent.com/ANiraula/PensionModeling/master/Database/reason.data.state.csv"
+reason.data <- read_csv(url(urlfile), col_names = TRUE, na = c(""), skip_empty_rows = TRUE, col_types = NULL)
+View(filteredData(reason.data, "Arkansas Teachers Retirement Plan", 2001))
